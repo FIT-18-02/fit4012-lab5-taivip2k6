adfa64f82c4cfe7999ac46875995938076abc3de
taivip2k6 <taibonxu10001@gmail.com>
2026-05-13 16:21:30 +0700

    Update decrypt.cpp

--- decrypt.cpp
+++ decrypt.cpp
@@ -1,11 +1,21 @@
-// Đảm bảo plaintext được thu hồi đúng cách
-unsigned char decrypted_output[16]; 
-// ... gọi hàm giải mã ...
+#include <iostream>
+#include "structures.h"
+using namespace std;
 
-cout << "Tin nhắn đã được giải mã: " << endl;
-for(int i = 0; i < 16; i++) {
-    if(decrypted_output[i] != 0) { // Loại bỏ zero padding khi in
-        cout << (char)decrypted_output[i];
+int main() {
+    // ... các đoạn code đọc file keyfile và message.aes đã có sẵn ...
+
+    // GIẢ SỬ: decrypted_output là mảng chứa kết quả sau khi gọi hàm giải mã
+    // Bạn cần thay tên biến cho đúng với code hiện tại của bạn
+    
+    cout << "Tin nhắn đã được giải mã: " << endl;
+    for(int i = 0; i < 16; i++) {
+        // Chỉ in các ký tự không phải là null (xử lý zero padding)
+        if(decrypted_output[i] != 0) { 
+            cout << (char)decrypted_output[i];
+        }
     }
+    cout << endl;
+
+    return 0;
 }
-cout << endl;

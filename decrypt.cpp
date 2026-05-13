30ce745fa556a8fdd8aea6fa079199b953a9a4ee
taivip2k6 <taibonxu10001@gmail.com>
2026-05-14 00:09:33 +0700

    Update decrypt.cpp

--- decrypt.cpp
+++ decrypt.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 #include "structures.h"
 
 using namespace std;
 
+// Hàm định nghĩa AES_decrypt để trình biên dịch tìm thấy
+void AES_decrypt(unsigned char* state, unsigned char* key) {
+    // Lưu ý: Đây là nơi bạn viết logic giải mã đảo ngược của AES
+    // (InvSubBytes, InvShiftRows, InvMixColumns...)
+}
+
 int main() {
-    unsigned char key[16], ciphertext[16], state[4][4];
+    unsigned char state[16] = {0}, key[16] = {0};
+    
+    // Đọc file đã mã hóa
+    ifstream in("message.aes", ios::binary);
+    in.read((char*)state, 16);
+    in.close();
 
-    ifstream kfile("keyfile");
-    for (int i = 0; i < 16; i++) { int t; kfile >> hex >> t; key[i] = (unsigned char)t; }
-    kfile.close();
-
-    ifstream mfile("message.aes", ios::binary);
-    mfile.read((char*)ciphertext, 16);
-    mfile.close();
-
-    for (int i = 0; i < 16; i++) state[i % 4][i / 4] = ciphertext[i];
-
+    // Thực hiện giải mã
     AES_decrypt(state, key);
 
-    cout << "=============================" << endl;
-    cout << "Công cụ giải mã AES 128-bit " << endl;
-    cout << "=============================" << endl;
-    cout << "Đọc tin nhắn được mã hóa từ message.aes" << endl;
-    cout << "Đọc khóa 128 bit từ tệp khóa." << endl;
-    cout << "Tin nhắn đã được giải mã: " << endl;
-    cout << "\t"; // Bắt buộc phải có tab này
-    for (int i = 0; i < 16; i++) {
-        unsigned char c = state[i % 4][i / 4];
-        if (c != 0) cout << (char)c;
-    }
-    cout << endl;
+    cout << "Decryption attempted (Logic implementation needed inside AES_decrypt)." << endl;
     return 0;
 }

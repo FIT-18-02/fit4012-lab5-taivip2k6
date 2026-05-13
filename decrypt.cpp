4064e09518249b575e8750385f06df984cbf6bc5
taivip2k6 <taibonxu10001@gmail.com>
2026-05-13 16:24:17 +0700

    Update decrypt.cpp

--- decrypt.cpp
+++ decrypt.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
+#include <fstream>
 #include "structures.h"
+
 using namespace std;
 
 int main() {
-    // ... các đoạn code đọc file keyfile và message.aes đã có sẵn ...
+    // Khai báo các mảng cần thiết
+    unsigned char key[16];
+    unsigned char ciphertext[16];
+    unsigned char decrypted_output[16]; // KHAI BÁO BIẾN Ở ĐÂY
 
-    // GIẢ SỬ: decrypted_output là mảng chứa kết quả sau khi gọi hàm giải mã
-    // Bạn cần thay tên biến cho đúng với code hiện tại của bạn
+    // Đọc khóa từ tệp 'keyfile'
+    ifstream kfile("keyfile");
+    if (kfile.is_open()) {
+        for (int i = 0; i < 16; i++) {
+            int temp;
+            kfile >> hex >> temp;
+            key[i] = (unsigned char)temp;
+        }
+        kfile.close();
+    }
+
+    // Đọc dữ liệu đã mã hóa từ 'message.aes'
+    ifstream mfile("message.aes", ios::binary);
+    if (mfile.is_open()) {
+        mfile.read((char*)ciphertext, 16);
+        mfile.close();
+    }
+
+    // TODO: Gọi hàm giải mã của bạn tại đây
+    // Ví dụ: AES_decrypt(ciphertext, key, decrypted_output);
     
+    // Tạm thời gán để tránh lỗi logic khi test nếu bạn chưa xong hàm giải mã
+    for(int i = 0; i < 16; i++) decrypted_output[i] = ciphertext[i];
+
+    // Phần in kết quả đã sửa lỗi
     cout << "Tin nhắn đã được giải mã: " << endl;
     for(int i = 0; i < 16; i++) {
-        // Chỉ in các ký tự không phải là null (xử lý zero padding)
         if(decrypted_output[i] != 0) { 
             cout << (char)decrypted_output[i];
         }
